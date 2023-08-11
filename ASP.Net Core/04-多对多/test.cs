58616c91fc84eed687f410107871b52d7d4d123a
liuxu <779227561@qq.com>
2023-08-11 20:02:04 +0800

    test:添加test测试文件

---
+++ ASP.Net Core/04-多对多/test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_多对多
+{
+    public class test
+    {
+        public string Name { get; set; } = "王一博";
+    }
+}
