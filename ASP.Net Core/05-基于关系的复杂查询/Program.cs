cd6b757d24955b8b79324080d437eec14b466026
liuxu <779227561@qq.com>
2023-08-12 12:20:29 +0800

    test：既生IEnumerable，何生亮IQueryable

--- ASP.Net Core/05-基于关系的复杂查询/Program.cs
+++ ASP.Net Core/05-基于关系的复杂查询/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _05_基于关系的复杂查询
+﻿using System.Xml.Linq;
+
+namespace _05_基于关系的复杂查询
 {
     internal class Program
     {
@@ -20,6 +22,26 @@
             //{
             //    Console.WriteLine(item.Title);
             //}
+
+            #region 既生IEnumerable，何生亮IQueryable
+            // 1、服务端评估（即在数据库中比对，效率更高）
+            IQueryable<Comment> result = ctx.Comments;
+            IQueryable<Comment> res = result.Where(d => d.Message.Contains("微软"));
+            // sql语句：SELECT [t].[Id], [t].[ArticleId], [t].[Message] FROM[T_Comment] AS[t] WHERE[t].[Message] LIKE N'%微软%'
+            foreach (Comment item in res)
+            {
+                Console.WriteLine(item.Message);
+            }
+
+            //// 2、客户端评估（即在内存中比对，效率较低）
+            //IEnumerable<Comment> result = ctx.Comments;
+            //IEnumerable<Comment> res = result.Where(d => d.Message.Contains("微软"));
+            //// sql语句：SELECT [t].[Id], [t].[ArticleId], [t].[Message] FROM[T_Comment] AS[t]
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item.Message);
+            //}
+            #endregion
         }
     }
 }
