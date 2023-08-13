d0bb34889c50e91a930dee7451fb7d91815c28a4
liuxu <779227561@qq.com>
2023-08-13 11:20:40 +0800

    test：分页查询

---
+++ ASP.Net Core/06-分页查询/ArticleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _06_分页查询
+{
+    public class ArticleConfig : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            //输出为那张表
+            builder.ToTable("T_Articles");
+            //设置字段在数据库中的属性
+            builder.Property(x => x.Title).HasMaxLength(100).IsUnicode().IsRequired();
+            builder.Property(x => x.Message).IsUnicode().IsRequired();
+        }
+    }
+}
---
+++ ASP.Net Core/06-分页查询/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _06_分页查询
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PriatPage(1, 3);
+        }
+
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="pageIndex">页码</param>
+        /// <param name="pageSize">每页数量</param>
+        private static void PriatPage(int pageIndex, int pageSize)
+        {
+            using MyDbContext ctx = new MyDbContext();
+            //查询所有符合条件的内容
+            IQueryable<Article> res = ctx.Articles.Where(d => d.Id > 0);
+            //分页查询（注意：这里不要链式调用，链式调用无法计算总条数和总页数）
+            IQueryable<Article> items = res.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            foreach (Article item in items)
+            {
+                Console.WriteLine(item.Title);
+            }
+            //查询总条数
+            long total = res.LongCount();
+            //查询总页数
+            long pageTotal = (long)Math.Ceiling(total * 1.0 / pageSize);
+            Console.WriteLine("总条数: " + total);
+            Console.WriteLine("总页数: " + pageTotal);
+        }
+    }
+}
