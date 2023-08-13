e834f1275dc499c32eb1ac4260eb771762bb50ef
liuxu <779227561@qq.com>
2023-08-13 15:12:54 +0800

    test：EF Core中的异步方法

--- ASP.Net Core/06-分页查询/Program.cs
+++ ASP.Net Core/06-分页查询/Program.cs
@@ -1,29 +1,73 @@
-﻿namespace _06_分页查询
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _06_分页查询
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            PriatPage(1, 3);
+            //PriatPage(1, 3);
             using MyDbContext ctx = new MyDbContext();
 
+            #region IQueryable读取数据的两种方法
             // 1、IQueryable分批从数据库读取数据；（内存占用小，数据库连接时间长）
-            IQueryable<Article> res = ctx.Articles.Where(d => d.Id > 0);
+            //IQueryable<Article> res = ctx.Articles.Where(d => d.Id > 0);
 
-            foreach (Article item in res)
-            {
-                Console.WriteLine(item.Title);
-                Thread.Sleep(10);
-            }
+            //foreach (Article item in res)
+            //{
+            //    Console.WriteLine(item.Title);
+            //    Thread.Sleep(10);
+            //}
 
             // 2、IQueryable从数据库读取所有数据，放置内容中，再进行遍历。（内存占用大，数据库连接时间短）
-            var res1 = ctx.Articles.Where(d => d.Id > 0).ToArray();
+            //var res1 = ctx.Articles.Where(d => d.Id > 0).ToArray();
 
-            foreach (Article item in res1)
-            {
-                Console.WriteLine(item.Title);
-                Thread.Sleep(10);
-            }
+            //foreach (Article item in res1)
+            //{
+            //    Console.WriteLine(item.Title);
+            //    Thread.Sleep(10);
+            //}
+            #endregion
+
+            #region EF Core中的异步方法
+            // 1、获取总条数
+            //int count = await ctx.Articles.CountAsync();
+            //Console.WriteLine(count);
+
+            // 2、新增数据
+            //Article article = new Article
+            //{
+            //    Title = "Lovely mylife",
+            //    Message = "热爱每一天",
+            //    Price = 120
+            //};
+            //await ctx.Articles.AddAsync(article);
+            //await ctx.SaveChangesAsync();
+
+            // 3、获取数据
+            //var res = await ctx.Articles.FirstAsync();
+            //Console.WriteLine(res.Title);
+
+            // 4、异步遍历数据
+            //var res = ctx.Articles.Where(d => d.Id > 0);
+            //常规遍历（一般情况下，常规遍历就可以解决问题，除非遇到性能瓶颈）
+            //foreach (Article item in res)
+            //{
+            //    Console.WriteLine(item.Title);
+            //}
+
+            //方式一：使用 终结方法的异步方法
+            //foreach (Article item in await res.ToArrayAsync())
+            //{
+            //    Console.WriteLine(item.Title);
+            //}
+
+            //方式二：使用 await foreach 方法
+            //await foreach (Article item in res.AsAsyncEnumerable())
+            //{
+            //    Console.WriteLine(item.Title);
+            //}
+            #endregion
         }
 
         /// <summary>
