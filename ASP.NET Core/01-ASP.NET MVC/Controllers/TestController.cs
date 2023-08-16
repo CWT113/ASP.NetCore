555610b69c19ce9bfd2e2805ed6a0289a5cc7475
liuxu <779227561@qq.com>
2023-08-16 21:09:14 +0800

    test：初试MVC项目

---
+++ ASP.NET Core/01-ASP.NET MVC/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using _01_ASP.NET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_ASP.NET_MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Demo()//Action方法
+        {
+            Person person = new Person("王一博", true,DateTime.Now);
+            return View(person);
+        }
+    }
+}
---
+++ ASP.NET Core/01-ASP.NET MVC/Models/Person.cs
@@ -0,0 +1,4 @@
+﻿namespace _01_ASP.NET_MVC.Models
+{
+    public record Person(string Name, bool IsVIP, DateTime DateTime);
+}
