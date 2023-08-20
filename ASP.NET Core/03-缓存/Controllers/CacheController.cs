d4bdcee7cee8c2af06d222ff2eed834f9349fbfd
王一博 <779227561@qq.com>
2023-08-20 15:32:44 +0800

    test：缓存

---
+++ ASP.NET Core/03-缓存/Controllers/CacheController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _03_缓存.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        //cache-control缓存响应头，10表示缓存内容10秒
+        [ResponseCache(Duration = 10)]
+        [HttpGet]
+        public DateTime GetNow()
+        {
+            return DateTime.Now;
+        }
+    }
+}
