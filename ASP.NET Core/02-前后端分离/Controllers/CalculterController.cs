6fc73392d628fdce8fe9e53173c6b0ac00b74e3f
liuxu <779227561@qq.com>
2023-08-20 11:07:41 +0800

    test：依赖注入

---
+++ ASP.NET Core/02-前后端分离/Controllers/CalculterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_前后端分离.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculterController : ControllerBase
+    {
+        //依赖注入
+        private readonly Calculate calculate;
+
+        public CalculterController(Calculate calculate)
+        {
+            this.calculate = calculate;
+        }
+
+        [HttpPost]
+        public int Add(int a, int b)
+        {
+            return calculate.Add(a, b);
+        }
+    }
+
+    public class Calculate
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- ASP.NET Core/02-前后端分离/Program.cs
+++ ASP.NET Core/02-前后端分离/Program.cs
@@ -1,3 +1,5 @@
+using _02_前后端分离.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//注入Calculate类
+builder.Services.AddScoped<Calculate>();
 
 //后端配置跨域
 builder.Services.AddCors(options =>
