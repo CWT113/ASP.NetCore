6e96592010176efb2775a05989aec0409623f3a0
王一博 <779227561@qq.com>
2023-08-23 21:15:00 +0800

    test：环境变量

---
+++ ASP.NET Core/04-环境变量/Controllers/EnvController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _04_环境变量.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EnvController : ControllerBase
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public EnvController(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public string? GetEnvironment()
+        {
+            //返回系统的环境变量
+            //return Environment.GetEnvironmentVariable("COUNT");
+
+            //返回开发环境/生产环境配置的环境变量
+            return webHostEnvironment.EnvironmentName;
+        }
+    }
+}
---
+++ ASP.NET Core/04-环境变量/Program.cs
@@ -0,0 +1,37 @@
+namespace _04_环境变量
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            //Console.WriteLine(app.Environment.EnvironmentName);//Development
+
+            //判断当前环境是否是 开发环境，开发环境下才启用 swagger
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
