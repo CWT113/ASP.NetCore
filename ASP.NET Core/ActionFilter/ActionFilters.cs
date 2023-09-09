2079ea4d09b990e44f5b1a63a118bcbd2823226f
王一博 <779227561@qq.com>
2023-09-09 12:19:44 +0800

    test：ActionFilter

---
+++ ASP.NET Core/ActionFilter/ActionFilters.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ActionFilter;
+
+public class ActionFilters2 : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        Console.WriteLine("AsyncActionFilter2:  之前执行！");
+        ActionExecutedContext result = await next();
+        if (result.Exception != null)
+        {
+            Console.WriteLine("AsyncActionFilter2： 发生异常");
+        }
+        else
+        {
+            Console.WriteLine("AsyncActionFilter2：  执行成功");
+        }
+    }
+}
---
+++ ASP.NET Core/ActionFilter/ActionFilters1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ActionFilter;
+
+public class ActionFilters : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        Console.WriteLine("AsyncActionFilter1:  之前执行！");
+        ActionExecutedContext result = await next();
+        if (result.Exception != null)
+        {
+            Console.WriteLine("AsyncActionFilter1： 发生异常");
+        }
+        else
+        {
+            Console.WriteLine("AsyncActionFilter1：  执行成功");
+        }
+    }
+}
