7b7986eab487584acd83788960740fe526c0f091
王一博 <779227561@qq.com>
2023-09-11 21:07:09 +0800

    test：简单的中间件

---
+++ ASP.NET Core/中间件/Program.cs
@@ -0,0 +1,27 @@
+using System.Reflection.PortableExecutable;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+//ÖÐ¼ä¼þ
+app.Map("/test", async appbuilder => {
+    appbuilder.Use(async (context, next) => {
+        context.Response.ContentType = "text/html";
+        await context.Response.WriteAsync("1  Start<br/>");
+        await next.Invoke();
+        await context.Response.WriteAsync("1  End<br/>");
+    });
+    appbuilder.Use(async (context, next) => {
+        await context.Response.WriteAsync("2  Start<br/>");
+        await next.Invoke();
+        await context.Response.WriteAsync("2  End<br/>");
+    });
+    appbuilder.Run(async ctx => {
+        await ctx.Response.WriteAsync("hello middleware <br/>");
+    });
+});
+
+
+app.Run();
