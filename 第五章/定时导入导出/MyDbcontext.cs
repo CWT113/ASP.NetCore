715f3e46b5efa73d9a1a4ad4c22e73ae488346cb
LiuXu <liux@zy-cast.com>
2023-09-20 18:16:37 +0800

    test：定时导入导出案例

---
+++ 第五章/定时导入导出/MyDbcontext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity;
+
+public class MyDbcontext : IdentityDbContext<MyUser, MyRole, long>
+{
+    public MyDbcontext(DbContextOptions<MyDbcontext> options) : base(options)
+    {
+    }
+}
