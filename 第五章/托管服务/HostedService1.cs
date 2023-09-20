4636d66d3b04327675e20cbf07c5b03187fd00f3
LiuXu <liux@zy-cast.com>
2023-09-20 17:55:06 +0800

    test：托管服务

---
+++ 第五章/托管服务/HostedService1.cs
@@ -0,0 +1,24 @@
+﻿namespace 托管服务
+{
+    public class HostedService1 : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                Console.WriteLine("HostedService1启动");
+                await Task.Delay(3000);
+
+                string txt = await File.ReadAllTextAsync(@"D:\MyDemo\ASP.NetCore\第五章\1.txt");
+                Console.WriteLine("文件读取完成");
+
+                await Task.Delay(10000);
+                Console.WriteLine(txt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("异常：" + ex);
+            }
+        }
+    }
+}
---
+++ 第五章/托管服务/Program.cs
@@ -0,0 +1,30 @@
+using 托管服务;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//注册托管服务
+builder.Services.AddHostedService<HostedService1>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
