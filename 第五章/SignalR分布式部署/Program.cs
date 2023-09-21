41e7e9f82df758cbfc004ceda7af4cbbeafd885f
LiuXu <liux@zy-cast.com>
2023-09-21 18:14:43 +0800

    test：Redis bakplane

---
+++ 第五章/SignalR分布式部署/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+//注册微软官方消息队列：Redis bakplane
+builder.Services.AddSignalR().AddStackExchangeRedis("127.0.0.1", option =>
+{
+    option.Configuration.ChannelPrefix = "Web_SignalR_";
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
